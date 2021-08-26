1675b322d5e57e4c12d47ae6b0b4874d500a9070
kssonu1811 <ksajansonu@yahoo.com>
2021-08-27 03:48:24 +0530

    first commit

--- ticket/settings.py
+++ ticket/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os.path
 from pathlib import Path
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +142,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # whitenoise setting
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedMainfestStaticFilesStorage'
+
+django_heroku.settings(locals())
